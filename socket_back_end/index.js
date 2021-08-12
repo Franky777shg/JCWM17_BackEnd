242dbdbceeefb479a58fe8ce000260aeface0c18
Franky777shg <frengky.sihombing.777@gmail.com>
2021-08-12 11:54:53 +0700

    socket send message done

--- socket_back_end/index.js
+++ socket_back_end/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const http = require('http')
 const socketIO = require('socket.io')
 const PORT = 2000
@@ -7,13 +7,22 @@ const PORT = 2000
 const app = express()
 const server = http.createServer(app)
 
-// app.use(cors())
+app.use(cors())
+app.use(express.json())
 
 //socket configuration
 const io = socketIO(server)
 let arrMsg = []
 app.io = io
 app.arrMsg = arrMsg
+
+app.post('/sendmsg', (req, res) => {
+    console.log(req.body)
+    arrMsg.push(req.body)
+
+    io.emit('chat msg', arrMsg)
+    res.status(200).send(arrMsg)
+})
 
 io.on('connection', socket => {
     socket.on('JoinChat', data => {
--- socket_front_end/src/App.js
+++ socket_front_end/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Axios from 'axios'
 import {
   Container,
   InputGroup,
   FormControl,
-  Button
+  Button,
+  Table
 } from 'react-bootstrap'
 import NavigationBar from './component/navigationBar';
 import io from 'socket.io-client'
@@ -14,15 +16,53 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      msg: [],
+      username: ''
     }
   }
 
   onJoinChat = () => {
-    const username = this.refs.username.value
+    const username = this.state.username
     const socket = io(URL_API)
 
     socket.emit('JoinChat', { username })
+    socket.on('chat msg', updateMsg => {
+      console.log('receive socket: ', updateMsg)
+      this.setState({ msg: updateMsg })
+    })
+  }
+
+  onSend = () => {
+    const message = this.refs.message.value
+    const username = this.state.username
+
+    const body = {
+      username,
+      message
+    }
+    console.log(body)
+
+    Axios.post(`${URL_API}/sendmsg`, body)
+      .then(res => {
+        console.log(res.data)
+        alert('Message Send ✔')
+        this.refs.message.value = ""
+      })
+      .catch(err => {
+        console.log(err)
+        this.refs.message.value = ""
+      })
+  }
+
+  onRenderTBody = () => {
+    return this.state.msg.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item.username}</td>
+          <td>{item.message}</td>
+        </tr>
+      )
+    })
   }
 
   render() {
@@ -35,10 +75,28 @@ class App extends React.Component {
             <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
             <FormControl
               placeholder="Username"
-              ref="username"
+              onChange={(e) => this.setState({ username: e.target.value })}
             />
           </InputGroup>
           <Button variant="success" onClick={this.onJoinChat}>Join Chat</Button>
+          <Table striped bordered hover variant="dark" className="mt-3">
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Message</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.onRenderTBody()}
+            </tbody>
+          </Table>
+          <FormControl
+            as="textarea"
+            placeholder="Write your Message"
+            ref="message"
+            className="mb-3"
+          />
+          <Button variant="success" onClick={this.onSend}>Send Message</Button>
         </Container>
       </div>
     );
